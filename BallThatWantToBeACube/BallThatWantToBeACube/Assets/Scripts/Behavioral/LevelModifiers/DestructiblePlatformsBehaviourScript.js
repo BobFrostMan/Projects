9261d6ba6d3dcb1f62f7169af8d25a5e49f2e9be
BobFrostMan <KillingFrostMan@i.ua>
2015-05-29 03:20:45 +0300

    Added different models for destructible platforms. Performance improvements.

---
+++ BallThatWantToBeACube/BallThatWantToBeACube/Assets/Scripts/Behavioral/LevelModifiers/DestructiblePlatformsBehaviourScript.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+var secondsBeforeDestruction : float;
+public var destructibleObject : GameObject;
+public var destructibleObject2 : GameObject;
+public var destructibleObject3 : GameObject;
+public var zeroRotated : boolean;
+
+private var destructedObj : GameObject;
+
+function Start(){
+	var objectNumber = Random.value;
+	if (objectNumber <= 0.5){
+		destructedObj = destructibleObject;	
+	} 
+	if (objectNumber > 0.7) {
+		destructedObj = destructibleObject3;
+	}
+	if (objectNumber > 0.5 && objectNumber <= 0.7){
+		destructedObj = destructibleObject2;	
+	}
+}
+
+function OnCollisionEnter (col : Collision) {
+	if (destructibleObject && col.collider.gameObject.CompareTag("PlayerBall")){
+		yield WaitForSeconds(secondsBeforeDestruction);
+		var currentObj : GameObject;
+		if (zeroRotated){
+		  currentObj = Instantiate(destructedObj, transform.position, Quaternion.identity);
+		} else {
+		  currentObj = Instantiate(destructedObj, transform.position, transform.rotation);
+		}
+		var scale = transform.localScale;
+		currentObj.transform.localScale = scale;
+		Destroy(this.gameObject);
+		Debug.Log("Object " + gameObject.name  + " destructed!");
+	}
+}
