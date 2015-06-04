a1e8e29c29500e9e9079e876f9fbef1edf70b025
BobFrostMan <KillingFrostMan@i.ua>
2015-06-05 02:11:15 +0300

    Fixed BWTBQ5 issue. Added destroyer object script to Scene five. Potensial fix for BWTBQ 7

--- BallThatWantToBeACube/BallThatWantToBeACube/Assets/Scripts/Behavioral/CubeBehaviorScript.js
+++ BallThatWantToBeACube/BallThatWantToBeACube/Assets/Scripts/Behavioral/CubeBehaviorScript.js
@@ -1,4 +1,7 @@
 ﻿#pragma strict
+public var targetObject : GameObject;
+public var targetObjectPosition : Vector3; 
+public var message : Text;
 
 function OnTriggerEnter (other : Collider) {
 	if (other.gameObject.CompareTag("PlayerBall")){
@@ -7,10 +10,23 @@ function OnTriggerEnter (other : Collider) {
 		var mainCamera = GameObject.Find("Main Camera");
 		mainCamera.GetComponent(MainCameraController).player=this.gameObject;
 		this.gameObject.GetComponent(MovementControllerScript).speed = 15;
-		GameObject.Find("FinishLevel").gameObject.GetComponent(MeshRenderer).enabled = true;
+		//GameObject.Find("FinishLevel").gameObject.GetComponent(MeshRenderer).enabled = true;
+		if (targetObject){
+			if (!targetObjectPosition.Equals(null)){
+				var target : GameObject = Instantiate(targetObject, targetObjectPosition, Quaternion.identity);
+				target.gameObject.GetComponent(EndLevelScript).finishText = message;
+			} else {
+				Debug.LogError("Target object position wasn't set");
+			}
+		} else {
+			Debug.LogError("Target object wasn't set");
+		}
 	}
 	if (other.gameObject.CompareTag("FinishLevel")){
 		other.gameObject.SetActive(false);
-		GameObject.Find("Wall").SetActive(false);
+		var wall : GameObject = GameObject.Find("Wall");
+		if (wall){
+			wall.SetActive(false);
+		}
 	}
 }
