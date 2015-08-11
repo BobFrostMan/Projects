f5b77f52eb525d2e0802fe30a64a2ed165fa855a
BobFrostman <killingfrostman@i.ua>
2015-08-12 01:54:21 +0300

    Added teleport and additional platforms

---
+++ BallThatWantToBeACube/BallThatWantToBeACube/Assets/Scripts/Behavioral/LevelModifiers/TeleportationScript.js
@@ -0,0 +1,10 @@
+﻿
+public var teleportationEndpoint : GameObject;
+
+function OnTriggerEnter (other : Collider) {
+	if(teleportationEndpoint){
+		other.transform.position = teleportationEndpoint.transform.position;
+	} else {
+		Debug.LogError("Endpoint wasn't found!");
+	}
+}
